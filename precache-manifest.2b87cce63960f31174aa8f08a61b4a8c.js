fd12247e37b22a3587dcdc4077c0129264f9f63e
Max Gillespie <MaxGillespie@Maxs-MacBook-Pro.local>
2018-12-17 14:45:09 -0600

    Updates

---
+++ precache-manifest.2b87cce63960f31174aa8f08a61b4a8c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "acc78cc8db19273751875aeec276a990",
+    "url": "/myEHR-website/static/media/blockchain_process.acc78cc8.png"
+  },
+  {
+    "revision": "237e75d48b7c8c21b03c",
+    "url": "/myEHR-website/static/css/main.1a275c28.chunk.css"
+  },
+  {
+    "revision": "e6d9ae099eed4cab9d66",
+    "url": "/myEHR-website/static/js/1.e6d9ae09.chunk.js"
+  },
+  {
+    "revision": "0dcb318e3618ea0c2ac7",
+    "url": "/myEHR-website/static/js/runtime~main.0dcb318e.js"
+  },
+  {
+    "revision": "36e6a06a4146af2f966ffcf021751724",
+    "url": "/myEHR-website/static/media/GeosansLight.36e6a06a.ttf"
+  },
+  {
+    "revision": "237e75d48b7c8c21b03c",
+    "url": "/myEHR-website/static/js/main.237e75d4.chunk.js"
+  },
+  {
+    "revision": "0eb778eb4b8c451ae435e0cba2d696bc",
+    "url": "/myEHR-website/static/media/appLoginScreenshot.0eb778eb.png"
+  },
+  {
+    "revision": "c254f7f07c5f5bdc2a9cd08478aa9ba4",
+    "url": "/myEHR-website/static/media/appRecordsScreenshot.c254f7f0.png"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/myEHR-website/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "e86e5d3bde8723a67204b0c38bdaf544",
+    "url": "/myEHR-website/static/media/medicalblock.e86e5d3b.png"
+  },
+  {
+    "revision": "fbac7ba9008d7fab55b6c0d35b04d6d4",
+    "url": "/myEHR-website/index.html"
+  }
+];
