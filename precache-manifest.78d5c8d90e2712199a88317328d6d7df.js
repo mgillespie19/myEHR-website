7d249164073b3c7d334daf7b643c470cf634baad
Max Gillespie <MaxGillespie@Maxs-MacBook-Pro.local>
2018-12-18 15:18:15 -0700

    Updates

---
+++ precache-manifest.78d5c8d90e2712199a88317328d6d7df.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "36e6a06a4146af2f966ffcf021751724",
+    "url": "./static/media/GeosansLight.36e6a06a.ttf"
+  },
+  {
+    "revision": "b7ff1a59ee46429ed1a8",
+    "url": "./static/css/main.251a76e1.chunk.css"
+  },
+  {
+    "revision": "48338b27aac5943bfb2b",
+    "url": "./static/js/1.48338b27.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "270547477d62c2a88fb32d0346500312",
+    "url": "./static/media/house.27054747.png"
+  },
+  {
+    "revision": "10ebfeba0177ad9358db92a32b1e901b",
+    "url": "./static/media/hospital.10ebfeba.png"
+  },
+  {
+    "revision": "b7ff1a59ee46429ed1a8",
+    "url": "./static/js/main.b7ff1a59.chunk.js"
+  },
+  {
+    "revision": "acc78cc8db19273751875aeec276a990",
+    "url": "./static/media/blockchain_process.acc78cc8.png"
+  },
+  {
+    "revision": "0eb778eb4b8c451ae435e0cba2d696bc",
+    "url": "./static/media/appLoginScreenshot.0eb778eb.png"
+  },
+  {
+    "revision": "c254f7f07c5f5bdc2a9cd08478aa9ba4",
+    "url": "./static/media/appRecordsScreenshot.c254f7f0.png"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "./static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "e86e5d3bde8723a67204b0c38bdaf544",
+    "url": "./static/media/medicalblock.e86e5d3b.png"
+  },
+  {
+    "revision": "fb4382ddebc532031a0690423ff09df3",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myEHR-website/precache-manifest.648201e448db7f54168e35130859c11d.js"
+  "./precache-manifest.78d5c8d90e2712199a88317328d6d7df.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myEHR-website/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
