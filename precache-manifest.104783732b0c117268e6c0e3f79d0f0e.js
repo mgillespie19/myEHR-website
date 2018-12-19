5cb18d16fa2bdcfe9629a09930494ce530b4ae9b
Max Gillespie <MaxGillespie@Maxs-MacBook-Pro.local>
2018-12-18 21:03:34 -0700

    Updates

---
+++ precache-manifest.104783732b0c117268e6c0e3f79d0f0e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "36e6a06a4146af2f966ffcf021751724",
+    "url": "/myEHR-website/static/media/GeosansLight.36e6a06a.ttf"
+  },
+  {
+    "revision": "d0f1e90521585eadae9c",
+    "url": "/myEHR-website/static/css/main.edd90132.chunk.css"
+  },
+  {
+    "revision": "c2ae3c5f26f82866d852",
+    "url": "/myEHR-website/static/js/1.c2ae3c5f.chunk.js"
+  },
+  {
+    "revision": "0dcb318e3618ea0c2ac7",
+    "url": "/myEHR-website/static/js/runtime~main.0dcb318e.js"
+  },
+  {
+    "revision": "270547477d62c2a88fb32d0346500312",
+    "url": "/myEHR-website/static/media/house.27054747.png"
+  },
+  {
+    "revision": "10ebfeba0177ad9358db92a32b1e901b",
+    "url": "/myEHR-website/static/media/hospital.10ebfeba.png"
+  },
+  {
+    "revision": "d0f1e90521585eadae9c",
+    "url": "/myEHR-website/static/js/main.d0f1e905.chunk.js"
+  },
+  {
+    "revision": "acc78cc8db19273751875aeec276a990",
+    "url": "/myEHR-website/static/media/blockchain_process.acc78cc8.png"
+  },
+  {
+    "revision": "0eb778eb4b8c451ae435e0cba2d696bc",
+    "url": "/myEHR-website/static/media/appLoginScreenshot.0eb778eb.png"
+  },
+  {
+    "revision": "c254f7f07c5f5bdc2a9cd08478aa9ba4",
+    "url": "/myEHR-website/static/media/appRecordsScreenshot.c254f7f0.png"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/myEHR-website/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "e86e5d3bde8723a67204b0c38bdaf544",
+    "url": "/myEHR-website/static/media/medicalblock.e86e5d3b.png"
+  },
+  {
+    "revision": "ea1fa84da2601e01e1c88d46954e257f",
+    "url": "/myEHR-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62eec2d1b9fc9f62215c3b948103de3d.js"
+  "/myEHR-website/precache-manifest.104783732b0c117268e6c0e3f79d0f0e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/myEHR-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
