1f5fba2b3131b6c9e58b15f65452e030ee670f0e
Max Gillespie <MaxGillespie@Maxs-MacBook-Pro.local>
2018-12-18 21:26:39 -0700

    got rid of basename

--- my-ehr/src/componets/Navbar/Navbar.jsx
+++ my-ehr/src/componets/Navbar/Navbar.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import './Navbar.css';
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 import { a } from 'react-router-dom';
+
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
 
 class Navbar extends Component {
     constructor(props){
@@ -30,15 +32,15 @@ class Navbar extends Component {
                     <div className="logo"><a href={process.env.PUBLIC_URL + '/'}>myEHR</a></div>
                     <div className="nav-spacing"/>
                     <BrowserRouter>
-                    <div className="navbar-items">
-                        <ul>
-                            <li><a href={process.env.PUBLIC_URL + '/'}> Home </a></li>
-                            <li onClick={this.state.scrollToAbout}><a href={process.env.PUBLIC_URL + '/about/'} >About</a></li>
-                            <li><a href={process.env.PUBLIC_URL + '/patient/'} >Patient</a></li>
-                            <li><a href={process.env.PUBLIC_URL + '/provider/'}>Provider</a></li>
-                            <li><a href={process.env.PUBLIC_URL + '/account/'}>Account</a></li>
-                        </ul>
-                    </div>
+                        <div className="navbar-items">
+                            <ul>
+                                <li><a href={process.env.PUBLIC_URL + '/'}> Home </a></li>
+                                <li onClick={this.state.scrollToAbout}><a href={process.env.PUBLIC_URL + '/about/'} >About</a></li>
+                                <li><a href={process.env.PUBLIC_URL + '/patient/'} >Patient</a></li>
+                                <li><a href={process.env.PUBLIC_URL + '/provider/'}>Provider</a></li>
+                                <li><a href={process.env.PUBLIC_URL + '/account/'}>Account</a></li>
+                            </ul>
+                        </div>
                     </BrowserRouter>
                 </nav>
             </header>
