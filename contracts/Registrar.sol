6a3fd5397892df6d5299c9a700b5d97d0d0f9fb4
louis smidt <lsmidt22@gmail.com>
2019-02-19 23:22:11 -0600

    finished draft of Registrar

--- contracts/Registrar.sol
+++ contracts/Registrar.sol
@@ -16,18 +16,42 @@ contract Registrar {
         string url;
     }
 
-
     mapping (address => Entity) entityInfo;
     mapping (address => string) nameAtAddress;
     mapping (address => address) entityAtContract;
+    mapping (string => address) entityByName;
 
-    constructor(string _name, address _addr, string _url) public {
-        entityInfo[msg.sender] = Entity(_name, _addr, _url);
+
+    //Registrar must be initialized with at least one Entity
+    constructor(string memory _name, address contractAddr, string memory _url) public {
+        entityInfo[msg.sender] = Entity(_name, contractAddr, _url);
         nameAtAddress[msg.sender] = _name;
-        entityAtContract[_addr] = msg.sender;
-        
+        entityAtContract[contractAddr] = msg.sender;
     } 
 
+    function setEntityContractAddr(address contractAddr) public {
+        entityInfo[msg.sender].entityContractAddr = contractAddr;
+        entityAtContract[contractAddr] = msg.sender;
+    }
 
+    function getEntitytByName(string memory name) public view returns (address) {
+        return entityByName[name];
+    }
+
+    function getEntityName(address addr) public view returns (string memory) {
+        return entityInfo[addr].name;
+    }
+
+    function getEntityContractAddr(address addr) public view returns (address) {
+        return entityInfo[addr].entityContractAddr;
+    }
+
+    function getEntityHost(address addr) public view returns (string memory) {
+        return entityInfo[addr].url;
+    }
+
+    function setAgentHost(string memory _url) public {
+        entityInfo[msg.sender].url = _url;
+    }
 
 }
